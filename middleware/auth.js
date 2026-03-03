597c8b23567e33f5f827888bad57b0ab09d0a8d7
yyy862013-debug <yyy862013@gmail.com>
2026-03-03 19:29:41 +0000

    uu

---
+++ middleware/auth.js
@@ -0,0 +1,8 @@
+const WHITELISTED_EMAILS = [
+  "mandalrabindra2081@gmail.com",        // ← your email
+  "friend1@gmail.com",    // ← friend's email
+];
+
+const ADMIN_EMAILS = [
+  "mandalrabindra2081@gmail.com",        // ← only you are admin
+];
